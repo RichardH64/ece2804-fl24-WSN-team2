bfdb546f1d5d5949fa3eea169dcc53c8baf8879d
Richard Harrison <rharrison41@vt.edu>
2024-11-21 12:23:13 -0500

    function declarations

--- ATmega328P_standalone_program/ATmega328P_standalone_program.ino
+++ ATmega328P_standalone_program/ATmega328P_standalone_program.ino
@@ -1,8 +1,22 @@
 #include "WSNLibrary.h"
 #include <SoftwareSerial.h>
 
-volatile bool flag
-volatile unsigned long timer2OverflowCount = 0;
+// INTERRUPT RELATED SECTION
+volatile bool timer2_30SecondsPassed = false;
+volatile unsigned long timer2_OverflowCount = 0;
+
+//===BEGIN PROGRAM===//
+SoftwareSerial bluetooth(BT_RXDPIN, BT_TXDPIN);   // RX, TX
+
+
+void setupTimer1();
+void setupTImer2();
+
+float getTemp();
+float getSolarVoltage();
+
+void PWM_run();
+void BT_run();
 
 void setup() {
 
@@ -10,4 +24,18 @@ void setup() {
 
 void loop() {
 
+}
+
+ISR(TIMER1_COMPA_vect) {
+
+}
+
+ISR(TIMER2_OVF_vect) {
+  timer2_OverflowCount++; // Increment overflow counter
+
+  // Timer2 overflows approximately every 16.384 ms with 16MHz clock and 1024 prescaler
+  if (timer2_OverflowCount >= 1832) {   // 30 Seconds
+    timer2_OverflowCount = 0;           // Reset overflow counter
+    timer2_30SecondsPassed = true;
+  }
 }
