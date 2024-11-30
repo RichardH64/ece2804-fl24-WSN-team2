dfd8dfecbae1a3e0bcc1f4328aad5e7890418fc3
Richard Harrison <rharrison41@vt.edu>
2024-11-30 13:17:46 -0500

    Getting the PWM back working (no interrupt)

--- WSNProgram/WSNProgram.ino
+++ WSNProgram/WSNProgram.ino
@@ -1,8 +1,9 @@
-#include "WSNLibrary.h"
 #include <SoftwareSerial.h>
+#include "WSNLibrary.h"
 
 //===BEGIN PROGRAM===//
-volatile int pwmValue = 124;                         // Start with 50% duty cycle
+int pwmValue = 100;                         // Start with 50% duty cycle
+unsigned long previousFeedbackMillis = 0;
 
 void setupTimer1();
 
@@ -17,7 +18,11 @@ void setup() {
 } 
 
 void loop() {
-
+  unsigned long currentMillis = millis(); 
+  if (currentMillis - previousFeedbackMillis >= PWM_INTERVAL) {
+    previousFeedbackMillis = currentMillis;
+    PWM_run();
+  }
 }
 
 void setupTimer1() {
@@ -30,15 +35,8 @@ void setupTimer1() {
   TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
   TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10);
 
-  // Set TOP value to 499 for exact 64kHz
-  ICR1 = 249;
-
-  // Set initial PWM duty cycle
-  OCR1A = pwmValue; // Compare value for OC1A
-  OCR1B = pwmValue; // Compare value for OC1B
-
-  // Enable Timer1 Compare Match Interrupt
-  TIMSK1 = (1 << OCIE1A); // Enable compare match interrupt for OCR1A
+  // Set TOP value to 249 for exact 64kHz
+  ICR1 = 249;  
 }
 
 void PWM_run() {
@@ -46,7 +44,10 @@ void PWM_run() {
   float measuredVoltageAtA0 = (sensorValue * 5.0) / 1023.0;                         // Step 2A: Convert the analog reading (0-1023) to voltage
   float measuredOutputVoltage = measuredVoltageAtA0 / PWM_VOLTAGEDIVIDERRATIO;      // Step 2B: Convert the measured voltage to the actual voltage
 
-  if (measuredOutputVoltage < PWM_VOLTAGETHRESHOLDDOWN) {                           // Step 3:  Adjust the PWM duty cycle
+  if (measuredOutputVoltage < PWM_VOLTAGETHRESHOLDRESET) {
+    pwmValue = PWM_MAX - PWM_MIN;
+  }
+  else if (measuredOutputVoltage < PWM_VOLTAGETHRESHOLDDOWN) {                           // Step 3:  Adjust the PWM duty cycle
     pwmValue += 1; // Increase duty cycle
   } 
   else if (measuredOutputVoltage > PWM_VOLTAGETHRESHOLDUP) {
@@ -55,10 +56,5 @@ void PWM_run() {
   
   pwmValue = constrain(pwmValue, PWM_MIN, PWM_MAX);                                 // Step 4:  Constrain PWM value between MINPWM and MAXPWM
   OCR1A = pwmValue;                                                                 // Step 5A: Update Compare Match Values (Update duty cycle for Pin 9)
-  OCR1B = pwmValue;                                                                 // Step 5:B Update Compare Match Values (Update duty cycle for Pin 10)
-}
-
-
-ISR(TIMER1_COMPA_vect) {
-  PWM_run();
+  OCR1B = pwmValue;                                                               // Step 5B: Update Compare Match Values (Update duty cycle for Pin 10)
 }
